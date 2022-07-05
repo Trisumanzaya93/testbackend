9d9d45d9888e1e25a90849d9be48e50d28f3fb00
Trisumanzaya93 <trisumanzaya93a@gmail.com>
2022-07-05 15:51:34 +0700

    first commit

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,7 +1,84 @@
 const model = require("../models/index");
-const response = require("../helper/response")
+const response = require("../helper/response");
+const { Op } = require("sequelize");
+const pagination = require("../helper/pagination");
 
 const getAllProduct = async (req,res)=>{
+    const { per_page, page,brand_id,group_model_id,model_id } = req.query;
+    const where = {};
+    const whereOr = [];
+    const limit = parseInt(per_page ?? 10);
+    const offset = parseInt((page ?? 1) * limit) - limit;
+  
+    if (brand_id) {
+        whereOr.push(
+          {
+            brand_id: {
+              [Op.like]: `%${brand_id}%`,
+            },
+          }
+        )
+      }
+      if (group_model_id) {
+        whereOr.push(
+          {
+            group_model_id: {
+              [Op.like]: `%${group_model_id}%`,
+            },
+          }
+        )
+      }
+      if (model_id) {
+        whereOr.push(
+          {
+            model_id: {
+              [Op.like]: `%${model_id}%`,
+            },
+          }
+        )
+      }
+      if (whereOr.length !== 0) where[Op.or] = whereOr;
+    try {
+        const result = await model.products.findAll({
+            where,
+              include: [
+                {
+                  model: model.merk,
+                  attributes:["brand"],
+                  as: "merk",
+                },
+                {
+                    model: model.models,
+                    attributes:["model"],
+                    as: "models",
+                },
+                {
+                    model: model.group_models,
+                    attributes:["grup_model"],
+                    as: "group_models",
+                },
+              ]
+        })
+        const totalPage= await model.products.findAndCountAll()
+        return pagination(res, req, {
+            data: result,
+            total: totalPage.count,
+            status: 200,
+            message: "get product succes",
+            limit,
+            offset,
+            query: req.query,
+          });
+    } catch (error) {
+        return response(res,{
+            status:500,
+            message:"terjadi error",
+            error
+        })
+    }
+}
+
+const getProductById = async (req,res)=>{
     try {
         const result = await model.products.findOne({
             where: {
@@ -41,7 +118,6 @@ const getAllProduct = async (req,res)=>{
 
 const getmerk =async (req,res)=>{
     try {
-
         const result = await model.merk.findAll()
         
         return response(res,{
@@ -121,4 +197,4 @@ const createProduct =async (req,res)=>{
         })
     }
     }
-module.exports={getAllProduct,createProduct, getmerk,getGroupModel,getModel}
+module.exports={getAllProduct,createProduct, getmerk,getGroupModel,getModel,getProductById}
---
+++ src/helper/pagination.js
@@ -0,0 +1,34 @@
+const pagination = (res, req, response ) => {
+    const {data, query, total, limit, offset, status, message, error} = response;
+      
+      const totalPage = Math.ceil(total / limit);
+      const path = `http://${req.get('host') + req.baseUrl + req.route.path}?page`;
+      console.log(path);
+      let {page} = req.query;
+      page = parseInt(page)
+      let queryString = ''
+      Object.keys(query).forEach((key) => {
+        if (key !== 'page' && key !== 'path') {
+          queryString += `&${key}=${query[key]}`;
+        }
+      });
+      const prevLink = (page !== 1 ? `${path}=${page - 1}${queryString}` : null);
+      const nextLink = (page !== totalPage ? `${path}=${page + 1}${queryString}` : null);
+  
+      const resultPrint = {
+        offset: offset ?? null,
+        limit: limit || null,
+        total_data: total || 0,
+        total_page: totalPage || 0,
+        perv_link: prevLink || null,
+        next_link: nextLink || null,
+        message: message || null,
+        status: status ?? 500,
+        err: error || null,
+        data: data ?? null,
+      };
+      
+    res.status(status || 500).json(resultPrint)
+    }
+    
+    module.exports = pagination;
